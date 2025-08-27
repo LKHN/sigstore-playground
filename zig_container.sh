42fe345fbfcec46aa34dbf6fe91d29a7de70f9ad
Elkhan Mammadli <elkhan.mammadli@protonmail.com>
2025-08-27 20:36:14 +0400

    ci(feat): add keyless signing with cosign
    Signed-off-by: Elkhan Mammadli <elkhan.mammadli@protonmail.com>

---
+++ zig_container.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -x
+
+tarball_path=${1?'No tarball path specified.'}
+
+ctr=$(buildah from scratch)
+mnt=$(buildah mount "$ctr")
+
+tar -xJf "$tarball_path" -C "$mnt" --strip-components=1
+
+buildah unmount "$ctr"
+buildah config --entrypoint '["/zig"]' "$ctr"
+buildah commit --rm "$ctr" sigstore-playground:zig-linux-amd64
